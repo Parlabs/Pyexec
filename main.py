9c3867fc0d65553d29eacdc6f2d70f1acca372b9
Parlabs <119888473+Parlabs@users.noreply.github.com>
2023-10-02 16:29:41 -0400

    Add files via upload
    First commit of final version

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
 
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'memcached'
+app.config['SECRET_KEY'] = 'super secret key'
+sess = Session()
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+@app.route('/result', methods=["post"])
+def result():
+    input_code = str(request.form.get("code"))
+
+    if input_code == "None":
+        input_code = ""
+
+        print(str(request.form.get('cursorPosition')))
+
+    """if request.form.get('aops') == 'ops':
+        input_code = input_code[:request.form.get('cursorPosition')] + "if __name__ == '__main__':" + input_code[:request.form.get('cursorPosition')]"""
+
+    session['input_code'] = input_code
+    return redirect('/create', code=302)
+
 @app.route('/create')
 def editor():
-    return render_template("editor.html")
+    code = str(session.get('input_code'))
+
+    return render_template("editor.html", code=code)
 
 @app.route('/explore')
 def explore():
